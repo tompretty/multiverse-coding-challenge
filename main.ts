07f5f08c7484674e4a529e91f4880a1c44ca1bfe
TomPretty <tompretty2@gmail.com>
2024-08-11 19:32:04 +0100

    add console summary

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 import { readInput } from "./input.ts";
+import { printSummary } from "./output.ts";
 import { simulateWorld } from "./simulate.ts";
 
 async function run() {
   const worldInput = await readInput();
   const worldOutput = simulateWorld(worldInput);
-
-  console.log({ worldOutput });
+  printSummary(worldOutput);
 }
 
 run();
---
+++ output.ts
@@ -0,0 +1,17 @@
+import { RobotOutput, WorldOutput } from "./world.ts";
+
+export function printSummary(worldOutput: WorldOutput) {
+  worldOutput.robots.forEach((r) => printRobotSummary(r));
+}
+
+// ---- Helpers ---- //
+
+function printRobotSummary(robot: RobotOutput) {
+  let summary = `(${robot.position.x}, ${robot.position.y} ${robot.position.direction})`;
+
+  if (robot.isLost) {
+    summary += " LOST";
+  }
+
+  console.log(summary);
+}
